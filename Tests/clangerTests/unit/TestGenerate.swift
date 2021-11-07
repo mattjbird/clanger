fd3bb18787f4d50f489e39f7fb0ec07fd7d357ac
Matthew Bird <matthew.james.bird@gmail.com>
2021-11-07 12:02:32 +0000

    First pass at a more intelligent generator test.
    We need to instead write a test lexer for assembly.

--- Tests/clangerTests/unit/TestGenerate.swift
+++ Tests/clangerTests/unit/TestGenerate.swift
@@ -7,7 +7,7 @@ import XCTest
 class testGenerator: XCTestCase {
   // MARK: Expressions
   func testConstants() {
-    self.testExpression( .integerConstant(9001), "movl    $9001, %eax" )
+    self.testExpression( .integerConstant(9001), "movl  $9001, %eax" )
   }
 
   func testUnaryOps() {
@@ -15,7 +15,7 @@ class testGenerator: XCTestCase {
       .unaryOp(.negation, .integerConstant(3)),
       """
       movl    $3, %eax
-      neg    %eax
+      neg     %eax
       """
     )
 
@@ -24,8 +24,8 @@ class testGenerator: XCTestCase {
       .unaryOp(.negation, .unaryOp(.negation, .integerConstant(842))),
       """
       movl    $842, %eax
-      neg    %eax
-      neg    %eax
+      neg     %eax
+      neg     %eax
       """
     )
   }
@@ -96,20 +96,89 @@ class testGenerator: XCTestCase {
     let out = TestOutputHandler()
     let gen = Generator(out)
     genF(gen)
-    XCTAssertEqual(out.value.removingIndentation(), expected.removingIndentation())
+    AssertAssemblyEqual(out.value, expected)
+  }
+
+  func testCustomAssert() {
+    AssertAssemblyEqual("", "")
+    AssertAssemblyEqual("a", "a")
+    AssertAssemblyEqual("ab", "ab")
+    AssertAssemblyEqual("abc", "abc")
+    AssertAssemblyEqual("abc  ", "abc")
+    AssertAssemblyEqual("     abc  ", "abc")
+
+    AssertAssemblyEqual("ab\n  c", "ab\nc")
+    AssertAssemblyEqual("ab\n  c\n  d", "ab\nc\nd")
+
+    XCTAssertFalse(assemblyEqual("a", "b"))
+    XCTAssertFalse(assemblyEqual("ab", "a"))
+    XCTAssertFalse(assemblyEqual("a", "abc"))
+    XCTAssertFalse(assemblyEqual("ab   a", "ab"))
   }
 }
 
-// We don't care about indentation here
-// TODO: need to replace indenations and also convert all 2+ spaces into 2 spaces
-extension String {
-  func removingIndentation() -> String {
-      return self.replacingOccurrences(
-        of: "\n[\\s]+",
-        with: "\n",
-        options: .regularExpression,
-        range: nil
-      )
-      .trimmingCharacters(in: .whitespacesAndNewlines)
+fileprivate func AssertAssemblyEqual(
+  _ string1: String,
+  _ string2: String,
+  file: StaticString = #filePath,
+  line: UInt = #line
+) {
+  if assemblyEqual(string1, string2) { return }
+  XCTFail("""
+    XCTAssertion fail: assembly not equal:
+
+    \(string1)
+
+    does not equal
+
+    \(string2)
+    """,
+    file: file,
+    line: line
+  )
+}
+
+// TODO: rewrite this
+// This is a sort of poor man's tokeniser which goes through each string in
+// linear time and ignores whitespace. We might consider switching to something
+// more heavy duty here, although keeping things as strings does make the tests
+// easier to read.
+// A major drawback of this way of checking for equality is that while we do
+// properly consider newlines, spaces which break up tokens are ignoed: e.g.
+// "ret" == "re  t", which is incorrect!
+fileprivate func assemblyEqual( _ string1: String, _ string2: String) -> Bool {
+  let string1 = Array(string1)
+  let string2 = Array(string2)
+  var index1 = 0
+  var index2 = 0
+
+  func getNextNonSpace(_ str: [Character], _ index: inout Int) -> Character? {
+    guard index < str.count else { return nil }
+    var c = str[index]
+    while c == " " {
+      index += 1
+      if index >= str.count { return nil }
+      c = str[index]
+    }
+    return c
   }
+
+  while (index1 < string1.count) && (index2 < string2.count) {
+    guard let char1 = getNextNonSpace(string1, &index1) else { break }
+    guard let char2 = getNextNonSpace(string2, &index2) else { break }
+    if char1 != char2 {
+      return false
+    }
+    index1 += 1
+    index2 += 1
+  }
+  while (index1 < string1.count) {
+    if !string1[index1].isWhitespace { return false }
+    index1 += 1
+  }
+  while (index2 < string2.count) {
+    if !string2[index2].isWhitespace { return false }
+    index2 += 1
+  }
+  return true
 }
